4ef48cf27f6aeb939a63a730b37150f95f49a742
elisagc <48531514+elisagc@users.noreply.github.com>
2019-05-12 16:01:43 +0200

    Update js_contacto.js

--- js/js_contacto.js
+++ js/js_contacto.js
@@ -1,6 +1,11 @@
 var boton=document.getElementById("enviar");
+var audio=document.getElementById("audio");
+
+audio.style.visibility="hidden";
 boton.addEventListener("click",mensaje); 
 
 function mensaje(){
     window.confirm("El formulario ha sido enviado, ¡GRACIAS!");
+    audio.style.visibility="visible";
+   
 }
