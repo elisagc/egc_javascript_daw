a09d478b58c74bd9aaa6b503820bd015a13ea6a7
elisagc <48531514+elisagc@users.noreply.github.com>
2019-05-12 21:43:53 +0200

    Update js_contacto.js

--- js/js_contacto.js
+++ js/js_contacto.js
@@ -1,11 +1,13 @@
 var boton=document.getElementById("enviar");
-var audio=document.getElementById("audio");
-
-audio.style.visibility="hidden";
 boton.addEventListener("click",mensaje); 
 
 function mensaje(){
     window.confirm("Gracias por contactar con nosotros ¡Disfruta la canción!");
-    audio.style.visibility="visible";
-   
+    musica();
 }
+
+function musica(){
+    var audio = new Audio('../img/sonido2.mp3');
+	audio.play();
+}
+
